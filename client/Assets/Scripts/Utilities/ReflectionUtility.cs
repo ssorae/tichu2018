3596242fda0ee4d3799e24101760f6cb05213457
Sorae <ipa.joa@gmail.com>
2018-02-11 00:28:20 +0900

    리플렉션 유틸리티 추가

---
+++ client/Assets/Scripts/Utilities/ReflectionUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EXBoardGame.Utilities
+{
+	public static class ReflectionUtility
+	{
+		public static IEnumerable<Type> GetAllTypesOf<T>()
+		{
+			return from domainAssem in AppDomain.CurrentDomain.GetAssemblies()
+					from assemType in domainAssem.GetTypes()
+					where assemType.IsSubclassOf(typeof(T))
+					select assemType;
+		}
+	}
+}
